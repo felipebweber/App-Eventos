40381af6319d6dc27eaf3831ad8eb7978b34435b
felipeweber <weberecomp@gmail.com>
2020-07-25 09:31:34 -0300

    created view controller events and table view.

--- App-Eventos/EventsViewController.swift
+++ App-Eventos/EventsViewController.swift
@@ -2,13 +2,10 @@
 //  ViewController.swift
 //  App-Eventos
 //
-//  Created by Felipe Weber on 25/07/20.
-//  Copyright © 2020 Felipe Weber. All rights reserved.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
+class EventsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
