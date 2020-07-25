bd3c577dc446217439454fd9b7a2d2d29d6f518a
felipeweber <weberecomp@gmail.com>
2020-07-25 10:39:08 -0300

    created event purchase screen

---
+++ App-Eventos/ConfirmEventViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ConfirmEventViewController.swift
+//  App-Eventos
+//
+//  Created by Felipe Weber on 25/07/20.
+//  Copyright © 2020 Felipe Weber. All rights reserved.
+//
+
+import UIKit
+
+class ConfirmEventViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
