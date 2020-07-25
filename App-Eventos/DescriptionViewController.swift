7944e980703ac61f6989f28b752c534ff21e83ef
felipeweber <weberecomp@gmail.com>
2020-07-25 10:24:13 -0300

    Created screen details

---
+++ App-Eventos/DescriptionViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DescriptionViewController.swift
+//  App-Eventos
+//
+
+import UIKit
+
+class DescriptionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
